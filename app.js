ba8fb28a70c74f5e354bb690e051d5986d1e1a88
dfa2307 <dan.franciscoo@gmail.com>
2025-07-20 10:49:52 -0300

    modified:   app.js

--- app.js
+++ app.js
@@ -1,42 +1,28 @@
-// //Criando um variável para armazenar a tag H1 do HTML
-// let txtTitulo = document.querySelector('h1')
-// //Modificando a variável que esta armazenado o H1 do HTML, adicionando um texto
-// txtTitulo.innerHTML = "Bem vindo ao Jogo"
+let numGerado = gerarNumAleatorio();
+let numDigitado = parseInt(document.querySelector(`input`).value);
 
-// let txtParagrafo = document.querySelector('p')
-// txtParagrafo.innerHTML = "Digite um número de 1 a 10"
-
-//------------------------------------------------------------------------------------------------------------
-
-function modificarHTML (tag, texto){
-    let tagModificar = document.querySelector(tag)
-    tagModificar.innerHTML = texto
+function editarHTML(tag, texto) {
+  let tagHTML = document.querySelector(tag);
+  tagHTML.innerHTML = texto;
 }
 
-modificarHTML(`p`, `Digite um número de 1 a 10`)
-modificarHTML(`h1`, `Bem vindo ao Jogo`)
+editarHTML(`h1`, `Bem Vindo ao Jogo`);
+editarHTML(`p`, `Digite um número de 1 a 10:`);
 
-
-function gerarNumeroAleatorio(){
-    let numAleatorio = Math.floor(Math.random() * 10) + 1
-    return numAleatorio
+function gerarNumAleatorio() {
+  let numAleatorio = Math.floor(Math.random() * 10) + 1;
+  return numAleatorio;
 }
 
-let numeroAleatorio = gerarNumeroAleatorio()
-
-function verificarNumeroAleatorio(){
-    let numDigitado = document.querySelector('input').value
-    
-    if (numDigitado > numeroAleatorio){
-        modificarHTML(`h1`, `Você Errou!`)
-        modificarHTML(`p`, `Número digitado maior que o número secreto`)
-    }else if(numDigitado < numeroAleatorio){
-        modificarHTML(`h1`, `Você Errou!`)
-        modificarHTML(`p`, `Número digitado menor que o número secreto`)
-    }else{
-        modificarHTML(`h1`, `Parabéns`)
-        modificarHTML(`p`, `Você acertou o número secreto`)
-    }
+function verificarNumeroAleatorio() {
+  if (numDigitado == numGerado) {
+    editarHTML(`h1`, `Você Acertou`);
+    editarHTML(`p`, ``);
+  } else if (numDigitado < numGerado) {
+    editarHTML(`h1`, `Você Errou`);
+    editarHTML(`p`, `Número digitado menor que o número secreto`);
+  } else {
+    editarHTML(`h1`, `Você Errou`);
+    editarHTML(`p`, `Número digitado maior que o número secreto`);
+  }
 }
-
-
