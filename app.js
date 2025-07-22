ae024ec9406529700a717ece2618d5cd8319fafe
dfa2307 <dan.franciscoo@gmail.com>
2025-07-21 22:59:48 -0300

    modified:   app.js

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-let numGerado = gerarNumAleatorio();
-let numDigitado = parseInt(document.querySelector(`input`).value);
-
 function editarHTML(tag, texto) {
   let tagHTML = document.querySelector(tag);
   tagHTML.innerHTML = texto;
@@ -11,14 +8,18 @@ editarHTML(`p`, `Digite um número de 1 a 10:`);
 
 function gerarNumAleatorio() {
   let numAleatorio = Math.floor(Math.random() * 10) + 1;
-  return numAleatorio;
+  return numAleatorio
 }
 
+let numSecreto = gerarNumAleatorio()
+
 function verificarNumeroAleatorio() {
-  if (numDigitado == numGerado) {
+  let numDigitado = document.querySelector(`input`);
+
+  if (numDigitado.value == numSecreto) {
     editarHTML(`h1`, `Você Acertou`);
     editarHTML(`p`, ``);
-  } else if (numDigitado < numGerado) {
+  } else if (numDigitado.value < numSecreto) {
     editarHTML(`h1`, `Você Errou`);
     editarHTML(`p`, `Número digitado menor que o número secreto`);
   } else {
